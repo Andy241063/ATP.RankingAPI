9021fa34d45c1e23206b3de5ea7193aa5404bab3
Andre Santos <andrefonseca.info@gmail.com>
2026-06-30 18:49:42 -0300

    chore: adicionando alguns pontos ao player

--- ATP.RankingAPI.Domain/Entities/TennisPlayer.cs
+++ ATP.RankingAPI.Domain/Entities/TennisPlayer.cs
@@ -1,15 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace ATP.RankingAPI.Domain.Entities
 {
     public class TennisPlayer
     {
         public int Id { get; set; }
-        public string? Name { get; set; }
+        public required string Name { get; set; }
+        public int Age { get; set; }
+        public required string Country { get; set; }
         public int RankingScore { get; set; }
     }
 }
--- ATP.RankingAPI.Infrastructure/Impl/PlayerRepository.cs
+++ ATP.RankingAPI.Infrastructure/Impl/PlayerRepository.cs
@@ -14,7 +14,7 @@ namespace ATP.RankingAPI.Infrastructure.Impl
 
             connection.Open();
 
-            string query = "SELECT id, name, ranking_score FROM player WHERE Id = @Id";
+            string query = "SELECT id, name, age, country, ranking_score FROM player WHERE Id = @Id";
 
             using var command = new MySqlCommand(query, connection);
 
@@ -30,6 +30,8 @@ namespace ATP.RankingAPI.Infrastructure.Impl
                 {
                     Id = reader.GetInt32("id"),
                     Name = reader.GetString("name"),
+                    Age = reader.GetInt32("age"),
+                    Country = reader.GetString("country"),
                     RankingScore = reader.GetInt32("ranking_score")
                 };
 
@@ -45,7 +47,7 @@ namespace ATP.RankingAPI.Infrastructure.Impl
 
             connection.Open();
 
-            string query = "SELECT id, name, ranking_score FROM player";
+            string query = "SELECT id, name, age, country, ranking_score FROM player";
 
             using var command = new MySqlCommand(query, connection);
 
@@ -59,6 +61,8 @@ namespace ATP.RankingAPI.Infrastructure.Impl
                 {
                     Id = reader.GetInt32("id"),
                     Name = reader.GetString("name"),
+                    Age = reader.GetInt32("age"),
+                    Country = reader.GetString("country"),
                     RankingScore = reader.GetInt32("ranking_score")
                 };
                 playerList.Add(player);
@@ -72,13 +76,17 @@ namespace ATP.RankingAPI.Infrastructure.Impl
     
             connection.Open();
     
-            string query = "INSERT INTO player (id, name, ranking_score) VALUES (@Id, @Name, @RankingScore)";
+            string query = "INSERT INTO player (id, name, age, ranking_score) VALUES (@Id, @Name, @Age, @RankingScore)";
     
             using var command = new MySqlCommand(query, connection);
 
             command.Parameters.AddWithValue("@Id", player.Id);
 
             command.Parameters.AddWithValue("@Name", player.Name);
+
+            command.Parameters.AddWithValue("@Age", player.Age);
+
+            command.Parameters.AddWithValue("@Country", player.Country);
 
             command.Parameters.AddWithValue("@RankingScore", player.RankingScore);
     
@@ -91,12 +99,14 @@ namespace ATP.RankingAPI.Infrastructure.Impl
     
             connection.Open();
     
-            string query = "UPDATE player SET name = @Name, ranking_score = @RankingScore WHERE id = @Id";
+            string query = "UPDATE player SET name = @Name, age = @Age, ranking_score = @RankingScore WHERE id = @Id";
     
             using var command = new MySqlCommand(query, connection);
     
             command.Parameters.AddWithValue("@Id", id);
             command.Parameters.AddWithValue("@Name", player.Name);
+            command.Parameters.AddWithValue("@Age", player.Age);
+            command.Parameters.AddWithValue("@Country", player.Country);
             command.Parameters.AddWithValue("@RankingScore", player.RankingScore);
     
             command.ExecuteNonQuery();
